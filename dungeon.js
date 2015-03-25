5c8cef6b345abe95555fb660e2ecd4e57f572116
Sam <dogmeethorse@yahoo.com>
2015-03-25 16:22:40 -0400

    dungeon.js added

---
+++ dungeon.js
@@ -0,0 +1,20 @@
+var game.dungeon = {
+	levels : [];
+	scheduler : new ROT.Scheduler.Simple(),
+	engine : new ROT.Engine(this.scheduler);
+}
+var game.dungeon.createLevel = function(){
+
+}
+
+var game.dungeon.addLevel = function(){
+
+}
+
+var game.dungeon.clearLevel = function(){
+
+}
+
+var game.dungeon.renderLevel = function(){
+
+}
